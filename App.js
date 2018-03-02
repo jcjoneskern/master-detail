f3ef93df04639d73f16fa656eacfac3028c339eb
J-C <jcjoneskern@gmail.com>
2018-03-02 13:20:57 -0500

    skeleton

--- App.js
+++ App.js
@@ -1,15 +1,17 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { StackNavigator } from 'react-navigation';
+import Master from './Master';
+import Detail from './Detail';
 
-export default class App extends React.Component {
+const RootStack = StackNavigator({
+  Master: { screen: Master },
+  Detail: { screen: Detail }
+});
+
+export default class App extends Component {
   render() {
-    return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
-      </View>
-    );
+    return <RootStack />;
   }
 }
 
---
+++ Master.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import { Text, View } from 'react-native';
+
+export default class Master extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = { isLoading: true }
+    }
+
+    render() {
+        return (
+        <View>
+            <Text>Master</Text>
+        </View>
+        );
+    }
+}
